8225f91a414786946d48b4f7585a9a22ec24d768
Samima-Akter-Tithy <tithysamima789@gmail.com>
2022-12-08 01:34:57 +0600

    Update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
 }
